69ca09eadfb28c27aff8f3aebba2f6425f155547
yeomsungpil <present7256@gmail.com>
2023-12-01 10:11:44 +0900

    Initial Commit

---
+++ SeSACRecap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACRecap
+//
+//  Created by 염성필 on 2023/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
